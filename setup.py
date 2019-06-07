a8dc1d78154e8d6d1a100f95da63449002fb7732
Diana Huang <dkh@edx.org>
2019-06-07 15:56:59 -0400

    Update versions of opaque-keys.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="edx_user_state_client",
-    version="1.1.0",
+    version="1.1.1",
     packages=[
         "edx_user_state_client",
     ],
     install_requires=[
         "PyContracts>=1.7.1,<2.0.0",
-        "edx-opaque-keys>=0.2.0,<1.0.0",
+        "edx-opaque-keys>=1.0.0,<2.0.0",
         "xblock>=0.4,<2.0.0",
     ],
     classifiers=[
